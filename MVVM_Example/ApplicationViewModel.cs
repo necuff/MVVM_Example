f1f0a58f569a01a9247acf41eae6da38a7ca3572
Mikhail Zaykov <necuff87@gmail.com>
2021-11-08 12:33:01 +0700

    Add deleting command

--- MVVM_Example/ApplicationViewModel.cs
+++ MVVM_Example/ApplicationViewModel.cs
@@ -29,6 +29,25 @@ namespace MVVM_Example
             }
         }
 
+        // команда удаления
+        private RelayCommand removeCommand;
+        public RelayCommand RemoveCommand
+        {
+            get
+            {
+                return removeCommand ??
+                  (removeCommand = new RelayCommand(obj =>
+                  {
+                      Phone phone = obj as Phone;
+                      if (phone != null)
+                      {
+                          Phones.Remove(phone);
+                      }
+                  },
+                 (obj) => Phones.Count > 0));
+            }
+        }
+
         public Phone SelectedPhone
         {
             get { return selectedPhone; }
